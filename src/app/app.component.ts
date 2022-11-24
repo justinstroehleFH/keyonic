2882f6c0bb3e3fd75de312d3284f42d6409936df
nicotomasini <nico.tomasini@hotmail.com>
2022-11-24 16:39:35 +0100

    first logic implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,29 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
+  public labels = [
+    { labelName: 'All', icon: 'mail' },
+    { labelName: 'Work', icon: 'trash' },
+    { labelName: 'Uni', icon: 'paper-plane' },
+    { labelName: 'Shopping', icon: 'heart' },
+  ];
+
+  public displayLabels = this.labels.map((label) => {
+    return {
+      title: label.labelName,
+      url: `/folder/${label.labelName}`,
+      icon: label.icon,
+    };
+  });
+
+  // TODO Icons adden ??
+  /*   public appPages = [
+    { title: 'All', url: '/folder/all', icon: 'mail' },
+    { title: 'Work', url: '/folder/Outbox', icon: 'paper-plane' },
     { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
     { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
     { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  ]; */
   constructor() {}
 }
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -9,10 +9,25 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder!: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  public passworstArray = [
+    { name: 'FHV', password: '1234', url: 'www.ilias/fhv.at', label: 'Uni' },
+    { name: 'ZARA', password: 'z12z', url: 'www.zara.at', label: 'Shopping' },
+    { name: 'GIT', password: 'git4ever', url: 'www.github.com', label: 'Work' },
+    {
+      name: 'ProTask',
+      password: 'proNeverGonnaDie',
+      url: 'www.protask.com',
+      label: 'Work',
+    },
+  ];
+
+  public displayPasswords: any = [];
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    this.displayPasswords = this.passworstArray.filter((e) =>
+      this.folder !== 'All' ? e.label === this.folder : e.label != ''
+    );
   }
-
 }
