1078d57a5d20a4fddb6112eb66b4374306be66c0
nicotomasini <nico.tomasini@hotmail.com>
2022-11-30 09:38:57 +0100

    added pw genrator

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -17,6 +17,18 @@ export class DetailsPage implements OnInit {
   }
 
   generatePassword() {
-    console.log('generate');
-  }
+
+   const base = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let password = '';
+   for (let index = 0; index < 20; index++) {
+    const randomChar = base.charAt(this.getRandomInt(base.length));
+    password = password.concat(randomChar);
+   }
+      console.log(password);
+    }
+
+    getRandomInt(max: number) {
+      return Math.floor(Math.random() * max);
+    }
+
 }
