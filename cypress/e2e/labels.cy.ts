f5b804dfd6fda3ad2169052ba1c046fe568c9f67
nicotomasini <nico.tomasini@hotmail.com>
2023-01-19 10:36:15 +0100

    cypress testing

--- cypress/e2e/labels.cy.ts
+++ cypress/e2e/labels.cy.ts
@@ -1,5 +1,23 @@
-describe('template spec', () => {
+describe('create Label', () => {
   it('passes', () => {
     cy.visit('');
+    cy.get('[data-cy=openLabelModal]').click();
+
+    cy.get('[data-cy=labelNameInput]').type('Alex liebt Lucky Dragon');
+    cy.get('[data-cy=labelSelectorInput]').click();
+
+    cy.get(
+      '#alert-input-2-0 > .alert-button-inner > .alert-radio-label'
+    ).click();
+    cy.get('.alert-button-group > :nth-child(2)').click();
+    cy.get('[data-cy=saveLabel]').click();
+  });
+});
+
+describe('delete Label', () => {
+  it('passes', () => {
+    cy.visit('');
+    cy.get(':nth-child(5) > .item-has-start-slot').rightclick();
+    cy.get('.content-sizing > .list-md > :nth-child(2)').click();
   });
 });
